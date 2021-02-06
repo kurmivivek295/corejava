466bd810b49d778e5d7f3d5a0830338d3c1f7f71
Vivek Kurmi <kurmivivek295@gmail.com>
2021-02-07 00:05:36 +0530

    Add Data Structure and other programs

---
+++ src/oops/Interface1.java
@@ -0,0 +1,31 @@
+package oops;
+
+public interface Interface1 {
+    void m1();
+
+    default int m2() {
+        System.out.println("interface 1");
+        return 1;
+    }
+}
+interface Interface2 extends Interface1 {
+    void m1();
+
+    default int m2() {
+        System.out.println("interface 2");
+        return 2;
+    }
+}
+
+class Impl implements Interface2 {
+    public static void main(String[] args) {
+        Interface1 impl = new Impl();
+        impl.m2();
+        impl.m1();
+    }
+
+    @Override
+    public void m1() {
+        System.out.println("Impl");
+    }
+}
